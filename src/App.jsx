5fa37e7f15ace629aa77b2839d8d87d7e96d92bc
Ishan Mishra <ishan_mishra@icloud.com>
2024-03-01 10:57:09 +0530

    changed default movie name

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    searchMovies('Spiderman');
+    searchMovies('Interstellar');
 
   }, []);
   const searchMovies = async (title) => {
